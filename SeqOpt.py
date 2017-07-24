ab33b51b3a7b4fa27fec0cc86b186a55a126f5f6
adityaradhakrishnan <adityaradhakrishnan@gmail.com>
2017-07-24 10:02:14 -0400

    Made a python script with boilerplate

---
+++ SeqOpt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+## ----------------------------------------
+## Sequence Optimzer
+## ----------------------------------------
+## Quickly modify codon usage in a given
+## sequence subject to provided weights
+## ----------------------------------------
+## Author:  Aditya Radhakrishnan
+## Contact: adityaradhakrishnan@gmail.com
+## Website: radhakrishnan.me
+## Date:    2017/07/23
+## ----------------------------------------
